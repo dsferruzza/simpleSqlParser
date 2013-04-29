70662e16aa7ae0e8602bad28e3f13669a934f912
David Sferruzza <david.sferruzza@gmail.com>
2013-04-30 00:34:01 +0200

    Add tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,48 @@
 
 test('trim', function () {
-	expect(5);
+	expect(8);
 
-	equal(trim('test'), 'test');
-	equal(trim('test '), 'test');
-	equal(trim(' test'), 'test');
-	equal(trim(' test '), 'test');
-	equal(trim('test test'), 'test test');
+	deepEqual(trim('test'), 'test');
+	deepEqual(trim('test '), 'test');
+	deepEqual(trim(' test'), 'test');
+	deepEqual(trim(' test '), 'test');
+	deepEqual(trim('test test'), 'test test');
+	
+	var integer = 5;
+	var array = [0, 1, 2];
+	var object = {test: "test", great_aswer: 42};
+	deepEqual(trim(integer), integer);
+	deepEqual(trim(array), array);
+	deepEqual(trim(object), object);
+});
+
+test('protect/unprotect', function () {
+	expect(9);
+	
+	deepEqual(protect('test'), '#t#e#s#t#');
+	deepEqual(protect('#t#e#s#t#'), '###t###e###s###t###');
+	
+	deepEqual(unprotect('#t#e#s#t#'), 'test');
+	deepEqual(unprotect('###t###e###s###t###'), '#t#e#s#t#');
+	
+	var string = "this is a (complex) string, with some special chars like !:@.#ù%$^\
+	and also a line break";
+	deepEqual(unprotect(protect(string)), string);
+	
+	deepEqual(protect(''), '#');
+	deepEqual(unprotect('#'), '');
+	deepEqual(protect('#'), '###');
+	deepEqual(unprotect('###'), '#');
+});
+
+test('protect_split', function () {
+	expect(7);
+	
+	deepEqual(protect_split(',', 'test'), ['test']);
+	deepEqual(protect_split(',', 'test(1,2)'), ['test(1,2)']);
+	deepEqual(protect_split(',', 'test1,test2'), ['test1', 'test2']);
+	deepEqual(protect_split(',', 'test1,(test2,test3)'), ['test1', '(test2,test3)']);
+	deepEqual(protect_split(',', 'test1,"test2,test3"'), ['test1', '"test2,test3"']);
+	deepEqual(protect_split(',', 'test1,\'test2,test3\''), ['test1', '\'test2,test3\'']);
+	deepEqual(protect_split(',', 'test1,`test2,test3`'), ['test1', '`test2,test3`']);
 });
