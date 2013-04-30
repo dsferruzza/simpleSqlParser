7ab2847c1f9c3c3b091f4137de758dd177fbf282
David Sferruzza <david.sferruzza@gmail.com>
2013-04-30 10:37:58 +0200

    Add tests

--- tests/tests.js
+++ tests/tests.js
@@ -48,3 +48,161 @@ test('protect_split', function () {
 	deepEqual(protect_split(',', 'test1,\'test2,test3\''), ['test1', '\'test2,test3\'']);
 	deepEqual(protect_split(',', 'test1,`test2,test3`'), ['test1', '`test2,test3`']);
 });
+
+test('condition lexer', function () {
+	expect(17);
+
+	deepEqual(CondLexer.tokenize('column = othercolumn'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column=othercolumn'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column                 = \
+		othercolumn'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column = othertable.othercolumn'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othertable.othercolumn'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column <= othertable.othercolumn'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '<='},
+		{type: 'word', value: 'othertable.othercolumn'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column = "string"'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '='},
+		{type: 'string', value: 'string'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column = FUNCTION("string")'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'FUNCTION("string")'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column = FUNCTION("string", columns,"otherstring"       ,\
+		"string with SQL stuff like = AND OR ()")'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'FUNCTION("string", columns,"otherstring"       ,\
+		"string with SQL stuff like = AND OR ()")'},
+	]);
+
+	deepEqual(CondLexer.tokenize('table.column != "string with SQL stuff like = AND OR ()"'), [
+		{type: 'word', value: 'table.column'},
+		{type: 'operator', value: '!='},
+		{type: 'string', value: 'string with SQL stuff like = AND OR ()'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column = othercolumn AND column < 2'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '<'},
+		{type: 'word', value: '2'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column = othercolumn AND column < 2 OR column = "string"'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '<'},
+		{type: 'word', value: '2'},
+		{type: 'logic', value: 'OR'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'string', value: 'string'},
+	]);
+
+	deepEqual(CondLexer.tokenize('(column = othercolumn AND column < 2) OR column = "string"'), [
+		{type: 'group', value: '('},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '<'},
+		{type: 'word', value: '2'},
+		{type: 'group', value: ')'},
+		{type: 'logic', value: 'OR'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'string', value: 'string'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column = othercolumn (AND column < 2 OR column = "string")'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'group', value: '('},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '<'},
+		{type: 'word', value: '2'},
+		{type: 'logic', value: 'OR'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'string', value: 'string'},
+		{type: 'group', value: ')'},
+	]);
+
+	deepEqual(CondLexer.tokenize('(column = othercolumn)'), [
+		{type: 'group', value: '('},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'group', value: ')'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column = othercolumn (AND column < 2 OR (column = "string" AND table.othercolumn))'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'word', value: 'othercolumn'},
+		{type: 'group', value: '('},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '<'},
+		{type: 'word', value: '2'},
+		{type: 'logic', value: 'OR'},
+		{type: 'group', value: '('},
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: '='},
+		{type: 'string', value: 'string'},
+		{type: 'logic', value: 'AND'},
+		{type: 'word', value: 'table.othercolumn'},
+		{type: 'group', value: ')'},
+		{type: 'group', value: ')'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column IS NULL'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: 'IS'},
+		{type: 'word', value: 'NULL'},
+	]);
+
+	deepEqual(CondLexer.tokenize('column IS NOT NULL'), [
+		{type: 'word', value: 'column'},
+		{type: 'operator', value: 'IS'},
+		{type: 'operator', value: 'NOT'},
+		{type: 'word', value: 'NULL'},
+	]);
+});
