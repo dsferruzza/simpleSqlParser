f8c4238160a7c4ade3cff67246037e3c60708e79
David Sferruzza <david.sferruzza@gmail.com>
2013-10-11 16:44:52 +0200

    Add watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,21 @@ module.exports = function(grunt) {
       },
     },
     qunit: {
-      all: ['tests/**/*.html']
-    }
+      all: ['tests/**/*.html'],
+    },
+    watch: {
+      files: ['**.js'],
+      tasks: ['default'],
+      options: {
+        atBegin: true,
+        interrupt: true,
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['qunit', 'jshint']);
 
