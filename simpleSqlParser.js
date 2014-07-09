c6849a930c567bcfa19ddd9b50c330ea6c2353f5
Joshua Cole <joshuafcole@gmail.com>
2014-07-09 16:41:47 -0700

    Converts spaces to tabs

--- simpleSqlParser.js
+++ simpleSqlParser.js
@@ -747,12 +747,12 @@
 			if (typeof cond.operator != 'undefined') {
 				result += ' ' + cond.operator;
 				if (typeof cond.right != 'undefined') {
-          if (cond.operator === 'IN'){
-            result += ' (' + cond.right + ')';
-          }
-          else {
-            result += ' ' + cond.right;
-          }
+					if (cond.operator === 'IN') {
+						result += ' (' + cond.right + ')';
+					}
+					else {
+						result += ' ' + cond.right;
+					}
 				}
 			}
 		}
