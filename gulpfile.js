58a42c65c3c1d2aea58df1830c829fde93ebf0a6
David Sferruzza <david.sferruzza@gmail.com>
2014-07-13 00:13:04 +0200

    Build minified scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 
@@ -11,13 +13,15 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', function() {
+gulp.task('mocha', function() {
 	return gulp.src('./tests/test*.js')
 		.pipe(mocha({
 			ui: 'qunit',
 			reporter: 'spec',
 		}));
 });
+
+gulp.task('test', ['lint', 'mocha']);
 
 gulp.task('browserifyWithDeps', function() {
 	var bundler = browserify('./simpleSqlParser.js');
@@ -37,4 +41,18 @@ gulp.task('browserifyWithoutDeps', function() {
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['lint', 'test', 'browserifyWithDeps', 'browserifyWithoutDeps']);
+gulp.task('uglifyWithDeps', ['browserifyWithDeps'], function() {
+	return gulp.src('dist/simpleSqlParser.js')
+		.pipe(uglify())
+		.pipe(rename('simpleSqlParser.min.js'))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('uglifyWithoutDeps', ['browserifyWithoutDeps'], function() {
+	return gulp.src('dist/simpleSqlParser.withoutDeps.js')
+		.pipe(uglify())
+		.pipe(rename('simpleSqlParser.withoutDeps.min.js'))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['test', 'uglifyWithDeps', 'uglifyWithoutDeps']);
